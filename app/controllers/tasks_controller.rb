5ae6ac480e00ab0e9bd976d9294d0d0887f0790f
Sarah Alexander <sjalex78@gmail.com>
2021-11-16 15:36:08 +1100

    CRUD actions for task list with refractoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,43 @@
 class TasksController < ApplicationController
 
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+  @task = Task.find(params[:id])
+  @task.update(task_params)
+  redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task =Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@task)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :detail, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index", as: :tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  delete "tasks/:id", to: "tasks#destroy"
 end
